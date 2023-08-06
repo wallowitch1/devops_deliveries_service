9c768fa90d12364894db61f8e78a580b9e88bbba
wallowitch2 <wallowitch2@gmail.com>
2023-08-06 18:07:30 +0900

    add path

--- deliveries/index.js
+++ deliveries/index.js
@@ -5,6 +5,7 @@ const db = require('./models');
 const deliveryRouter = require('./routes/deliveries');
 const fs = require('fs');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3002;
